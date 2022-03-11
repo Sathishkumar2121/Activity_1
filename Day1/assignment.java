6248a9e3391779a5dfdc521ecde72de7e61fd915
Sathishkumar2121 <98815691+Sathishkumar2121@users.noreply.github.com>
2022-03-11 21:58:39 +0530

    Update assignment.java

--- Day1/assignment.java
+++ Day1/assignment.java
@@ -1,1 +1,18 @@
-
+import java.util.*;
+import java.io.*;
+ class Assignment
+ {
+    public static void main(String[] args)throws IOException
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        sc.close();
+       String[] nameArray = s1.split(" ");
+       String name=nameArray[0];
+       String lastName = s1.substring(s1.indexOf(" ") + 1).toUpperCase();
+        String n=name.substring(0, 1).toUpperCase();
+        String s3=n+name.substring(1);
+      String s4=s3+" "+lastName;
+        System.out.println(s4);
+    }
+}
